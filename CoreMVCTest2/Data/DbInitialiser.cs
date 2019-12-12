62a3c47e1505b2e855b88161cee1d71c34fb5e27
joniilosele <joniilosele@gmail.com>
2019-12-12 12:16:44 +0000

    add db initialiser class

---
+++ CoreMVCTest2/Data/DbInitialiser.cs
@@ -0,0 +1,31 @@
+﻿using CoreMVCTest2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVCTest2.Data
+{
+    public static class DbInitialiser
+    {
+        public static void Initialise(ApplicationDbContext context)
+        {
+            //InitialiseAddresses(context);
+        }
+
+        private static void InitialiseAddresses(ApplicationDbContext context)
+        {
+            if(context.Address.Any())
+            {
+                return;
+            }
+
+            var addresses = new Address[]
+            {
+                new Address
+                {
+                }
+            };
+        }
+    }
+}
--- CoreMVCTest2/Program.cs
+++ CoreMVCTest2/Program.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreMVCTest2.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CoreMVCTest2
 {
@@ -14,7 +16,26 @@ namespace CoreMVCTest2
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    DbInitialiser.Initialise(context);
+                }
+
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
